1945dbf94385d42c21ad79289bdc19c09541c2da
shahaknir <62562867+shahaknir@users.noreply.github.com>
2021-05-21 17:23:07 +0300

    Medic update

--- src/Medic.hpp
+++ src/Medic.hpp
@@ -18,6 +18,20 @@ namespace pandemic{
 
         Medic(Board b, City c);
 
+        // Returns Player's Role in the Game - has no use in Player
+        string role();
+
+        // When treating - get disease level to 0
+        Player& treat(City city);
+
+        // If cure was discovered - get disease level to 0
+        // to each city he is in
+        Player& drive(City city);
+        Player& fly_direct(City city);
+        Player& fly_charter(City city);
+        Player& fly_shuttle(City city);
+
+
     };
 
 }
